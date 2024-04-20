87fdd8cdb496b9bf6796040ede8b75df2ba737a1
kila <luka.kilasonia.1@btu.edu.ge>
2024-04-20 15:58:44 +0400

    update api call functions

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -1,11 +1,10 @@
-import axios, { AxiosResponse } from "axios";
-import { UserProfileResponse } from "../Types/ApiTypes/UserProfile";
+import axios from "axios";
 
-const setLocalAccesToken = (token: string) => {
+export const setLocalAccesToken = (token: string) => {
   localStorage.setItem("spotify-access-token", token);
 };
 
-const setLocalRefreshToken = (refToken: string) => {
+export const setLocalRefreshToken = (refToken: string) => {
   localStorage.setItem("spotify-refresh-token", refToken);
 };
 
@@ -14,35 +13,26 @@ const getLocalRefreshToken = () =>
   localStorage.getItem("spotify-refresh-token");
 
 //set Access token for the first time , after authentication.
-export const setAccessToken = async (code: string) => {
+export const setAccessToken = (code: string) => {
   const url = "https://accounts.spotify.com/api/token";
 
   //client ID and secret string
   const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
   const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
 
-  const body = {
+  const form = {
     code: code,
-    redirect_uri: "http://localhost:5173/",
+    redirect_uri: "http://localhost:5173/profile",
     grant_type: "authorization_code",
   };
 
   const headers = {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
-    },
-    json: true,
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
   };
 
   // post request for acces token
-  await axios.post(url, body, headers).then((d) => {
-    if (d.status === 200 && d.data) {
-      console.log(d.data);
-      setLocalAccesToken(d.data.access_token);
-      setLocalRefreshToken(d.data.refresh_token);
-    }
-  });
+  return axios.post(url, form, { headers });
 };
 
 // get Access token
@@ -54,20 +44,46 @@ export const getAccessToken = () => {
   }
 };
 
+const headers = {
+  Authorization: "Bearer " + getAccessToken(),
+  "Content-Type": "application/json",
+};
+
 // get user profile
 
-export const getUserProfile = async () => {
+export const getUserInfo = () => {
   const url = "https://api.spotify.com/v1/me";
-  const accessToken = getAccessToken();
 
-  if (accessToken) {
-    const response: AxiosResponse<UserProfileResponse> = await axios.get(url, {
-      headers: {
-        Authorization: "Bearer " + getAccessToken(),
-        "Content-Type": "application/json",
-      },
-    });
+  return axios.get(url, {
+    headers,
+  });
+};
 
-    return response;
-  }
+// get UserFollowedArtists
+
+export const getUserFollowing = () => {
+  return axios.get(
+    "https://api.spotify.com/v1/me/following?type=artist&limit=10",
+    {
+      headers,
+    }
+  );
+};
+
+export const getUserPlaylist = () => {
+  return axios.get("https://api.spotify.com/v1/me/playlists", { headers });
+};
+
+// all data for User Profile
+
+export const getUserProfile = () => {
+  return axios.all([getUserInfo(), getUserFollowing(), getUserPlaylist()]).then(
+    axios.spread((user, following, playlist) => {
+      return {
+        user: user?.data,
+        following: following?.data.artists.total,
+        playlist: playlist?.data.total,
+      };
+    })
+  );
 };
