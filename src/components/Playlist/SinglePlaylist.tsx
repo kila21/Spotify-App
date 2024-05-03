c5a98aaa830ad3bc1d92c6163b59174c0ebe19a1
kila <luka.kilasonia.1@btu.edu.ge>
2024-05-03 14:50:51 +0400

    add dependency

--- src/components/Playlist/SinglePlaylist.tsx
+++ src/components/Playlist/SinglePlaylist.tsx
@@ -1,12 +1,33 @@
 import styled from "styled-components";
-import { useEffect } from "react";
-import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { getPlaylistTracks } from "../../Api/Api";
+import { TracksItem } from "../../Types/ApiTypes/getTopTracksResponse";
+import { SinglePlaylistTrack } from "../../Types/ApiTypes/getPlaylistResponse";
 
 export const SinglePlaylist = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [tracks, setTracks] = useState<null | TracksItem[]>();
+
   useEffect(() => {
-    console.log(location);
-  }, []);
+    getPlaylistTracks(location.state.tracks.href).then((d) => {
+      if (d.status === 200) {
+        const newArray: TracksItem[] = [];
+        d.data.items.map((item: SinglePlaylistTrack) => {
+          const newData = {
+            ...item.track,
+          };
+          newArray.push(newData);
+        });
+        setTracks(newArray);
+      }
+    });
+  }, [location.state.tracks.href]);
+
+  const clickHandler = (id: string) => {
+    navigate(`/top-tracks/${id}`);
+  };
   return (
     <SinglePlaylistStyled>
       <SinglePlaylistHeader>
@@ -25,6 +46,27 @@ export const SinglePlaylist = () => {
           get recomendation
         </SinglePlaylistRecomendation>
       </SinglePlaylistHeader>
+
+      <SinglePlaylistTracksStyled>
+        {tracks &&
+          tracks.map((t, i) => (
+            <TracksListItem onClick={() => clickHandler(t.id)} key={t.name + i}>
+              <TracksListItemImage src={t.album.images[0].url} />
+              <TracksListItemContent>
+                <TracksListItemName>{t.name}</TracksListItemName>
+                <TrackListItemArtists>
+                  {t.artists.map((a) => a.name).join(", ")}
+                </TrackListItemArtists>
+              </TracksListItemContent>
+              <TracksDuration>
+                {Math.floor(t.duration_ms / 60000)} :{" "}
+                {Math.floor((t.duration_ms / 1000) % 60) < 10
+                  ? "0" + Math.floor((t.duration_ms / 1000) % 60)
+                  : Math.floor((t.duration_ms / 1000) % 60)}
+              </TracksDuration>
+            </TracksListItem>
+          ))}
+      </SinglePlaylistTracksStyled>
     </SinglePlaylistStyled>
   );
 };
@@ -58,7 +100,7 @@ const SinglePlaylistHeader = styled.div`
   min-height: 150px;
   margin-top: 50px;
   @media (min-width: 769px) {
-    width: 30%;
+    width: 40%;
     margin-left: 50px;
     height: 450px;
   }
@@ -105,4 +147,57 @@ const SinglePlaylistRecomendation = styled.button`
     height: 30px;
     font-size: 14px;
   }
+`;
+
+const SinglePlaylistTracksStyled = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 30px;
+  margin-top: 50px;
+  overflow-y: scroll;
+`;
+
+const TracksListItem = styled.li`
+  width: 100%;
+  max-height: 50px;
+  list-style: none;
+  display: flex;
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
+
+const TracksListItemImage = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 20px;
+`;
+
+const TracksListItemContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 70%;
+
+  height: 100%;
+`;
+
+const TracksListItemName = styled.h3`
+  color: white;
+  font-size: 17px;
+`;
+
+const TrackListItemArtists = styled.p`
+  color: grey;
+`;
+
+const TracksDuration = styled.span`
+  height: 100%;
+  min-width: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 12px;
 `;
